21db5bc64b195d632c9f7715fa8dd4dbf861e532
Artur <artur.jesslen@epfl.ch>
2020-05-07 18:40:13 +0200

    CODE CLEAN - First part working

--- project/src/main.py
+++ project/src/main.py
@@ -1,8 +1,7 @@
 import argparse
 import cv2
-from functions import *
+from utils import find_objects, coor_object
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 parser = argparse.ArgumentParser(description='IAPR Special Project.')
@@ -17,85 +16,114 @@ parser.add_argument('--output',
 
 args = parser.parse_args()
 
+class Calculator:
+    """
+    Class that contains all functions necessary to compute the calculs
+    """
+    def __init__(self, input_path, output_path):
+        """
+        Initialization of the class
+        """
+        self.input_path = input_path
+        self.output_path = output_path
+        self.cap = None
+        self.out = None
+        self.object_position = None
+        self.arrow_position = None
+        self.arrow_color = None
 
-def find_object(frame):
-    """
-    Find object and arrow position in frame and return it
-    """
-    return frame, frame
+    def __enter__(self):
+        """
+        Importing the input video and creating the output video 
+        """
+        print('Importing file...')
+        # Using video from file:
+        self.cap = cv2.VideoCapture(filename=self.input_path)
+        # Define the codec and create VideoWriter object.
+        frame_width = int(self.cap.get(3))
+        frame_height = int(self.cap.get(4))
+        self.out = cv2.VideoWriter(filename=self.output_path, fourcc=cv2.VideoWriter_fourcc(
+            'M', 'J', 'P', 'G'), fps=2, frameSize=(frame_width, frame_height))
 
-def display_objects(frame, object_position, arrow_position):
-    """
-    Display object position on frame
-    """
-    size = np.array([20, 20])
-    # Draw digit and sign cases
-    object_position = [np.array(elt) for elt in object_position]
-    for pos in object_position:
-        top_left = pos - size
-        bottom_right = pos + size
+    def __exit__(self, type, value, traceback):
+        """
+        Releasing the input and output video
+        """
+        self.cap.release()
+        self.out.release()
+        cv2.destroyAllWindows()
+        print('Output video saved...')
+        self.cap = None
+        self.out = None
+
+    def process(self):
+        if self.cap is None or self.out is None:
+            print("ERROR : Use this function inside a with statement")
+            return
+        current_frame = 0
+        while self.cap.isOpened():
+            # Capture frame by frame
+            ret, frame = self.cap.read()
+            if ret:
+                print('Processing frame #{}'.format(current_frame))
+                if current_frame == 0:
+                    self.object_position, self.arrow_position, self.arrow_color = find_objects(frame)
+                else:
+                    self.find_arrow(frame)
+                
+                self.out.write(self.frame_display(frame))
+            else:
+                break
+            current_frame += 1
+
+    def find_arrow(self, frame):
+        """
+        Find the position of the arrow using the known color of the arrow
+        """
+        lower_red = np.array(self.arrow_color)*255 - np.array([70, 70, 70])
+        upper_red = np.array(self.arrow_color)*255 + np.array([70, 70, 70])
+        mask = cv2.inRange(frame, lower_red, upper_red)
+        closing = cv2.morphologyEx(
+            mask, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10)))
+        self.arrow_position = coor_object(closing)
+
+    def frame_display(self, frame):
+        """
+        Prepare frame for video
+        """
+        out = self.display_objects(frame)
+        return out
+
+    def display_objects(self, frame):
+        """
+        Display object position on frame
+        """
+        size = np.array([20, 20])
+        # Draw digit and sign cases
+        object_position = [np.array(elt) for elt in self.object_position]
+        for pos in object_position:
+            top_left = pos - size
+            bottom_right = pos + size
+            frame = cv2.rectangle(
+                frame, (int(top_left[0]), int(top_left[1])),
+                (int(bottom_right[0]), int(bottom_right[1])),
+                (255, 0, 0), thickness=2)
+        # Draw arrow case
+        top_left = np.array(self.arrow_position) - 2*size
+        bottom_right = np.array(self.arrow_position) + 2*size
         frame = cv2.rectangle(
             frame, (int(top_left[0]), int(top_left[1])),
             (int(bottom_right[0]), int(bottom_right[1])),
-            (255, 0, 0), thickness=2)
-    # Draw arrow case
-    top_left = np.array(arrow_position) - 2*size
-    bottom_right = np.array(arrow_position) + 2*size
-    frame = cv2.rectangle(
-        frame, (int(top_left[0]), int(top_left[1])),
-        (int(bottom_right[0]), int(bottom_right[1])),
-        (0, 0, 255), thickness=2)
-    return frame
-
-def find_arrow(frame):
-    return frame
-
-def process(frame):
-    """
-    Processing image
-    """
-    object_position, arrow_position = find_object(frame)
-    position_arrow = find_arrow(frame)
-    object_position = np.asarray([[283, 233], [468, 236], [
-        487, 127], [295, 104], [161, 94], [370, 205], [198, 285]])
-    arrow_position = [468, 300]
-    processed_frame = display_objects(frame, object_position, arrow_position)
-    return processed_frame
-
+            (0, 0, 255), thickness=2)
+        return frame
 
 def main():
     """
     main function
     """
-    print('Importing file...')
-    # Playing video from file:
-    cap = cv2.VideoCapture(filename=args.input)
-    # Define the codec and create VideoWriter object.
-    frame_width = int(cap.get(3))
-    frame_height = int(cap.get(4))
-    out = cv2.VideoWriter(filename=args.output, fourcc=cv2.VideoWriter_fourcc(
-        'M', 'J', 'P', 'G'), fps=2, frameSize=(frame_width, frame_height))
-    out2 = cv2.VideoWriter(filename='../results/robot_parcours_2.avi', fourcc=cv2.VideoWriter_fourcc(
-        'M', 'J', 'P', 'G'), fps=2, frameSize=(frame_width, frame_height))
-    try:
-        current_frame = 0
-        while cap.isOpened():
-            # Capture frame by frame
-            ret, frame = cap.read()
-            if ret:
-                print('Processing frame #{}'.format(current_frame))
-                processed_frame = process(frame)
-                out.write(processed_frame)
-            else:
-                break
-
-            # To stop duplicate images
-            current_frame += 1
-    finally:
-        cap.release()
-        out.release()
-        cv2.destroyAllWindows()
-        print('Done...')
+    calculator = Calculator(args.input, args.output)
+    with calculator:
+        calculator.process()
 
 
 if __name__ == '__main__':
--- project/src/utils.py
+++ project/src/utils.py
@@ -1,15 +1,12 @@
 from skimage.color import rgb2gray
-import matplotlib.pyplot as plt
 import numpy as np
 import skimage.filters as filt
-
-def process(frame):
-    # do processing HERE
-    return frame
-
+import heapq
 
 def threshold(img, th1 = None, th2 = None):
-    """Threshold function"""
+    """
+    Threshold function
+    """
     out = np.ones(np.shape(img))
     if th1 is not None:
         out[np.where(img < th1)] = 0
@@ -18,66 +15,79 @@ def threshold(img, th1 = None, th2 = None):
     return out
 
 def check(list, val):
-    """Function to check if a neighbor has label, if yes, it will return the label"""
+    """
+    Function to check if a neighbor has label, if yes, it will return the label
+    """
     if val > min(list) and min(list) > 0:
         return min(list)
     return False
 
 
 def labeling(img, label):
-    """Return an image with element corresponding to the given label"""
+    """
+    Return an image with element corresponding to the given label
+    """
     out = np.zeros(np.shape(img), dtype = np.uint)
     out[np.where(img == label)] = 1
     return out
 
 
-def coor_object(object):
-    nn_zero_col = np.any(object, 0)
-    nn_zero_row = np.any(object, 1)
-    col = np.linspace(0, len(object[0]) - 1, num=len(object[0]), dtype=int)
-    row = np.linspace(0, len(object) - 1, num=len(object), dtype=int)
+def coor_object(object_):
+    """
+    Return the coordinate of the center of an object
+    """
+    nn_zero_col = np.any(object_, 0)
+    nn_zero_row = np.any(object_, 1)
+    col = np.linspace(0, len(object_[0]) - 1, num=len(object_[0]), dtype=int)
+    row = np.linspace(0, len(object_) - 1, num=len(object_), dtype=int)
     x = np.mean(col[nn_zero_col])
     y = np.mean(row[nn_zero_row])
     return [x, y]
 
 def extract_object(image, label):
+    """
+    Return the binary img, number of pixel and coordinate 
+    of the center of an object with a given label
+    """
     out = np.zeros(np.shape(image))
     out[np.where(image == label)] = 1
-    # get amount of pixels forming the object
     pxl = sum(sum(out))
-    # get avg coordinates of object
     coor = coor_object(out)
     return out, pxl, coor
 
 
+def color_arrow(label, label_unique, frame, label_choice):
+    """
+    Return the color of an object (only used for arrow)
+    """
+    img, _, _ = extract_object(label, label_unique[label_choice])
+    return [(frame[:, :, i] * img).mean() for i in range(3)] 
+
+
 def find_objects(frame):
-    # reshape : remove black lines on the side (out of table)
-    image = np.delete(frame, range(100), axis=1)
-    image = np.delete(image, range(500, len(frame[0])), axis=1)
+    """
+    Return the coordinates of the digits, of the arrow and the average color of the arrow
+    """
     # from RGB colors to gray scale
-    image = rgb2gray(image)
-    # threshold on image to separate background from foreground
+    image = rgb2gray(frame)
+    ones = np.ones_like(image)
+    # reshape : remove black lines on the side (out of table)
+    image = np.delete(image, range(100), axis=1)
+    image = np.delete(image, range(image.shape[1]-100, image.shape[1]), axis=1)
+    # Threshold on image to separate background from foreground
     # We are using an adaptive threshold that find the best threshold depending of the illumination
     t = filt.threshold_otsu(image)
-    image_thres = threshold(image, t)
-
-    fig, axes = plt.subplots(1, 3, figsize=(6, 6))
-    axes[0].imshow(image)
-    axes[1].imshow(image_thres)
-
-    # do processing HERE
+    ones[:, 100:frame.shape[1]-100] = threshold(image, t)
+    image = ones
     new_label = 1
-    im_h = len(image_thres)
-    im_w = len(image_thres[0])
-
-    label = np.zeros((im_h,im_w), dtype=np.uint)
-
+    im_h, im_w = image.shape
+    label = np.zeros((im_h, im_w), dtype=np.uint)
     # Iterating on each pixels of the image
     nb_neighbors = 10
     for i in range(1, im_h - nb_neighbors):
         for j in range(1, im_w - nb_neighbors):
             # Labeling each pixels of the foreground with same value as its neighbor
-            if image_thres[i, j] == 0:
+            if image[i, j] == 0:
                 label_neighbors = np.max([label[i + a, j + b] for a in range(- nb_neighbors, nb_neighbors+1)
                                           for b in range(- nb_neighbors, nb_neighbors + 1)])
                 if label_neighbors != 0:
@@ -88,7 +98,6 @@ def find_objects(frame):
 
     # Merging of labels for objects that are containing different labels
     nb_neighbors2 = 2
-
     for i in range(1, im_h - nb_neighbors2):
         for j in range(1, im_w - nb_neighbors2):
             label_neighbors = [label[i + a, j + b] for a in range(- nb_neighbors2, nb_neighbors2 + 1)
@@ -96,29 +105,23 @@ def find_objects(frame):
             val_min = check(label_neighbors, label[i, j])
             if check(label_neighbors, label[i, j]):
                 label[np.where(label == label[i, j])] = val_min
-
-
-    axes[2].imshow(label)
-
     label_unique = np.unique(label)
     # Background is not an object so we subtract 1
     nb_objects = len(label_unique) - 1
-
-    print('Frame 0 : There are {:d} objects.'.format(nb_objects),'labels are', label_unique)
-
-    # plotting only object of a certain label
-    fig2, ax = plt.subplots(int(nb_objects / 5)+1, 5, figsize=(14, 8))
+    
+    # Calculating number of pixel per object and the position of the center
     nb_pixels = []
     avg_coor = []
     for label_choice in range(1,nb_objects+1):
-        img, pxl, coor = extract_object(label, label_unique[label_choice])
-        ax[int(label_choice / 5)][label_choice % 5].imshow(img)
-        ax[int(label_choice / 5)][label_choice % 5].set_title('%i pixels'%pxl)
-        ax[int(label_choice / 5)][label_choice % 5].scatter(coor[0], coor[1], s = 10, color='r', marker = 'o')
+        _, pxl, coor = extract_object(label, label_unique[label_choice])
         nb_pixels.append(pxl)
         avg_coor.append(coor)
-    plt.pause(100)
-
-    #return frame
-
+    
+    #remove line in middle
+    idx = heapq.nlargest(3, range(len(nb_pixels)), key=nb_pixels.__getitem__)[:2]
+    arrow_coord = avg_coor[idx[0]]
+    arrow_color = color_arrow(label, label_unique, frame, idx[0]+1)
+    avg_coor = [i for j, i in enumerate(avg_coor) if j not in idx]
+    
+    return avg_coor, arrow_coord, arrow_color
 
